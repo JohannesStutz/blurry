c4a0804dc0c71f773b947b8b281ebbbb34c92c40
Johannes Stutz <61063548+JohannesStutz@users.noreply.github.com>
2021-02-19 00:03:37 +0100

    added pixelate mode and better fitting blurred areas

--- blurry/_nbdev.py
+++ blurry/_nbdev.py
@@ -2,8 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"show_inline_img": "00_core.ipynb",
+index = {"show_cv2": "00_core.ipynb",
          "blur": "00_core.ipynb",
+         "pixelate": "00_core.ipynb",
          "find_faces": "00_core.ipynb",
          "blur_areas": "00_core.ipynb",
          "anonymize": "00_core.ipynb",
